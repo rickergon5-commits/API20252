574c582cf895a0745a151b2d3200dc3eb148c4a9
Rickergon <rickergon5@gmail.com>
2025-10-31 17:21:36 -0500

    Conflictos resueltos en app.js y rutas

--- src/app.js
+++ src/app.js
@@ -1,39 +1,11 @@
-<<<<<<< HEAD
-import express from 'express'
-//importar las rutas OJO
-=======
 import express from 'express';
 import cors from 'cors';  // 👈 Importa cors
 //importar las rutas
->>>>>>> 9a93302 (Descripción de los cambios realizados)
 import clientesRoutes from './routes/clientes.routes.js';
 import productosRoutes from './routes/productos.routes.js'; 
 import usuarioRoutes from './routes/usuarios.routes.js'; 
 import authRoutes from "./routes/auth.routes.js";
 
-<<<<<<< HEAD
-const app=express();
-// Para leer JSON
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // para formularios
-
-// Para leer form-data o x-www-form-urlencoded
-//app.use(express.urlencoded({ extended: true }));
-//indicar las rutas a utilizar OJO
-app.use("/api", authRoutes);
-app.use('/api', clientesRoutes);
-app.use('/api', productosRoutes); 
-app.use("/api", usuarioRoutes);
-
-
-app.use((req,resp,next)=>{
-    resp.status(400).json({
-      message:'Endpoint not fount'
-    })
-})
-
-export default app; 
-=======
 const app = express();
 
 // === CORS ===
@@ -61,4 +33,3 @@ app.use((req, resp, next) => {
 });
 
 export default app;
->>>>>>> 9a93302 (Descripción de los cambios realizados)
